4119ca8695c8d43ad35daadf5feabf025e25d357
enchan <enchant0130@gmail.com>
2026-04-26 20:02:16 +0900

    feat: #1 大釜の前でディスペンサーを作動させた際にのみ発動するビヘイビアを登録した

--- src/main/java/me/enchan/cauldronextraction/CauldronExtractionMod.java
+++ src/main/java/me/enchan/cauldronextraction/CauldronExtractionMod.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.CauldronBlock;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPointer;
 
 public class CauldronExtractionMod implements ModInitializer {
     public static final String ModId = "cauldron-extraction";
@@ -12,6 +18,26 @@ public class CauldronExtractionMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        //
+        var targetItem = Items.BUCKET;
+        var defaultBehavior = DispenserBlock.BEHAVIORS.get(targetItem);
+
+        DispenserBlock.registerBehavior(targetItem, new ItemDispenserBehavior() {
+            @Override
+            protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+                var world = pointer.world();
+                var facing = pointer.state().get(DispenserBlock.FACING);
+                var frontPos = pointer.pos().offset(facing);
+                var state = world.getBlockState(frontPos);
+
+                if (!(state.getBlock() instanceof CauldronBlock)) {
+                    return defaultBehavior.dispense(pointer, stack);
+                }
+
+                Logger.info("me at the cauldron");
+
+                return stack;
+            }
+        });
+
     }
 }
